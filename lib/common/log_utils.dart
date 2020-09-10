1084480954d72758209e6555981ababc36ac2410
fenghaolin <476400902@qq.com>
2020-09-10 15:06:39 +0800

    添加捕获异常

--- lib/common/log_utils.dart
+++ lib/common/log_utils.dart
@@ -70,14 +70,18 @@ class XFCustomTrace {
   }
 
   void _parseTrace() {
-    var traceString = this._trace.toString().split("\n")[0];
-    var indexOfFileName = traceString.indexOf(RegExp(r'[A-Za-z_]+.dart'));
-    var fileInfo = traceString.substring(indexOfFileName);
-    var listOfInfos = fileInfo.split(":");
-    this.fileName = listOfInfos[0];
-    this.lineNumber = int.parse(listOfInfos[1]);
-    var columnStr = listOfInfos[2];
-    columnStr = columnStr.replaceFirst(")", "");
-    this.columnNumber = int.parse(columnStr);
+    try {
+      var traceString = this._trace.toString().split("\n")[0];
+      var indexOfFileName = traceString.indexOf(RegExp(r'[A-Za-z_]+.dart'));
+      var fileInfo = traceString.substring(indexOfFileName);
+      var listOfInfos = fileInfo.split(":");
+      this.fileName = listOfInfos[0];
+      this.lineNumber = int.parse(listOfInfos[1]);
+      var columnStr = listOfInfos[2];
+      columnStr = columnStr.replaceFirst(")", "");
+      this.columnNumber = int.parse(columnStr);
+    } catch (e) {
+      print(e);
+    }
   }
 }
