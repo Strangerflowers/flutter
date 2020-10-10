19e86d6abfa7f6a0d3dd1560a1e0082e07f23276
zzq <3074127187@qq.com>
2020-10-10 17:17:00 +0800

    fix:修改选择商品的appbar样式

--- lib/pages/offer/add_quote_product_page.dart
+++ lib/pages/offer/add_quote_product_page.dart
@@ -14,13 +14,25 @@ class AddQuoteProductPage extends StatelessWidget {
       child: Scaffold(
         resizeToAvoidBottomInset: false,
         appBar: AppBar(
+          elevation: 0,
+          centerTitle: true,
           leading: IconButton(
-            icon: Icon(Icons.arrow_back),
+            icon: Icon(
+              Icons.arrow_back,
+              color: Color(0xFF242526),
+            ),
             onPressed: () {
               Navigator.pop(context);
             },
           ),
-          title: Text('报价'),
+          title: Text(
+            '报价',
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              color: Color(0xFF242526),
+            ),
+          ),
+          backgroundColor: Colors.white,
         ),
         body: GestureDetector(
           onTap: () {
