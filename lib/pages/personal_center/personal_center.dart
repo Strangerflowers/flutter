a2f44a88bf1276c6ad83fa06a33714dc4128b21e
zzq <3074127187@qq.com>
2020-10-19 16:04:45 +0800

    fix:个人中心，注释多余代码

--- lib/pages/personal_center/personal_center.dart
+++ lib/pages/personal_center/personal_center.dart
@@ -183,85 +183,85 @@ class PersonalCenter extends StatelessWidget {
     );
   }
 
-  // 我的订单标题
-  Widget _orderTitle() {
-    return Container(
-      margin: EdgeInsets.only(top: 10),
-      decoration: BoxDecoration(
-        color: Colors.white,
-        border: Border(
-          bottom: BorderSide(width: 1, color: Colors.black12),
-        ),
-      ),
-      child: ListTile(
-        leading: Icon(Icons.list),
-        title: Text('我的订单'),
-        trailing: Icon(Icons.keyboard_arrow_right),
-      ),
-    );
-  }
+  // // 我的订单标题
+  // Widget _orderTitle() {
+  //   return Container(
+  //     margin: EdgeInsets.only(top: 10),
+  //     decoration: BoxDecoration(
+  //       color: Colors.white,
+  //       border: Border(
+  //         bottom: BorderSide(width: 1, color: Colors.black12),
+  //       ),
+  //     ),
+  //     child: ListTile(
+  //       leading: Icon(Icons.list),
+  //       title: Text('我的订单'),
+  //       trailing: Icon(Icons.keyboard_arrow_right),
+  //     ),
+  //   );
+  // }
 
-  Widget _orderType() {
-    return Container(
-      margin: EdgeInsets.only(top: 5),
-      width: ScreenUtil().setWidth(750),
-      height: ScreenUtil().setHeight(150),
-      padding: EdgeInsets.only(top: 10),
-      color: Colors.white,
-      child: Row(
-        children: <Widget>[
-          Container(
-            width: ScreenUtil().setWidth(187),
-            child: Column(
-              children: <Widget>[
-                Icon(
-                  Icons.party_mode,
-                  size: 30,
-                ),
-                Text('待付款'),
-              ],
-            ),
-          ),
-          Container(
-            width: ScreenUtil().setWidth(187),
-            child: Column(
-              children: <Widget>[
-                Icon(
-                  Icons.query_builder,
-                  size: 30,
-                ),
-                Text('待发货'),
-              ],
-            ),
-          ),
-          Container(
-            width: ScreenUtil().setWidth(187),
-            child: Column(
-              children: <Widget>[
-                Icon(
-                  Icons.card_giftcard,
-                  size: 30,
-                ),
-                Text('待收货'),
-              ],
-            ),
-          ),
-          Container(
-            width: ScreenUtil().setWidth(187),
-            child: Column(
-              children: <Widget>[
-                Icon(
-                  Icons.content_paste,
-                  size: 30,
-                ),
-                Text('待评价'),
-              ],
-            ),
-          ),
-        ],
-      ),
-    );
-  }
+  // Widget _orderType() {
+  //   return Container(
+  //     margin: EdgeInsets.only(top: 5),
+  //     width: ScreenUtil().setWidth(750),
+  //     height: ScreenUtil().setHeight(150),
+  //     padding: EdgeInsets.only(top: 10),
+  //     color: Colors.white,
+  //     child: Row(
+  //       children: <Widget>[
+  //         Container(
+  //           width: ScreenUtil().setWidth(187),
+  //           child: Column(
+  //             children: <Widget>[
+  //               Icon(
+  //                 Icons.party_mode,
+  //                 size: 30,
+  //               ),
+  //               Text('待付款'),
+  //             ],
+  //           ),
+  //         ),
+  //         Container(
+  //           width: ScreenUtil().setWidth(187),
+  //           child: Column(
+  //             children: <Widget>[
+  //               Icon(
+  //                 Icons.query_builder,
+  //                 size: 30,
+  //               ),
+  //               Text('待发货'),
+  //             ],
+  //           ),
+  //         ),
+  //         Container(
+  //           width: ScreenUtil().setWidth(187),
+  //           child: Column(
+  //             children: <Widget>[
+  //               Icon(
+  //                 Icons.card_giftcard,
+  //                 size: 30,
+  //               ),
+  //               Text('待收货'),
+  //             ],
+  //           ),
+  //         ),
+  //         Container(
+  //           width: ScreenUtil().setWidth(187),
+  //           child: Column(
+  //             children: <Widget>[
+  //               Icon(
+  //                 Icons.content_paste,
+  //                 size: 30,
+  //               ),
+  //               Text('待评价'),
+  //             ],
+  //           ),
+  //         ),
+  //       ],
+  //     ),
+  //   );
+  // }
 
   // 通用ListTile
   Widget _myListTile(BuildContext context, String title, String url, item) {
